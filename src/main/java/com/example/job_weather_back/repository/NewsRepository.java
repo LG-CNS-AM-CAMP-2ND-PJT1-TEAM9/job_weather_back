6591e12c9b88e3fabecc7de81dc0bf3f0c7da597
문지욱 <93529343+moonjiuk@users.noreply.github.com>
2025-05-20 16:41:16 +0900

    feat: 뉴스 API 기능 구현

---
+++ src/main/java/com/example/job_weather_back/repository/NewsRepository.java
@@ -0,0 +1,19 @@
+package com.example.job_weather_back.repository;
+
+import com.example.job_weather_back.entity.News;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface NewsRepository extends JpaRepository<News, Integer> {
+    // 최신 뉴스 조회
+    Optional<News> findTopByOrderByNewsDateTimeDesc();
+
+    // 검색어가 포함된 뉴스 조회
+    List<News> findByNewsTitleContainingOrNewsDescriptionContaining(String title, String description);
+    
+    // 모든 뉴스를 날짜순으로 반환
+    List<News> findAllByOrderByNewsDateTimeDesc();
+} 
