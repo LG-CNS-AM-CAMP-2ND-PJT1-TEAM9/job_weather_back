b3cc5b6fd3e215b68f4efda4d0877fb23a4ca08a
Junseo Yoon <58902513+liljuice@users.noreply.github.com>
2025-05-22 00:45:23 +0900

    feat: 날씨 스케줄링(09시, 12시, 18시) 기능 추가

--- src/main/java/com/example/job_weather_back/JobWeatherBackApplication.java
+++ src/main/java/com/example/job_weather_back/JobWeatherBackApplication.java
@@ -4,9 +4,11 @@ import com.example.job_weather_back.config.GptProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GptProperties.class)
+@EnableScheduling
 public class JobWeatherBackApplication {
 
 	public static void main(String[] args) {
