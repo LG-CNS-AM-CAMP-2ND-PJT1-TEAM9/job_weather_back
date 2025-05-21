c956b385a2dacabb58579d307b244bf0b7dc3cb0
soobin70 <142298498+soobin70@users.noreply.github.com>
2025-05-22 02:00:34 +0900

    feat: 네이버 회원 탈퇴, 로그아웃 기능 구현

--- src/main/java/com/example/job_weather_back/repository/CustomizationRepository.java
+++ src/main/java/com/example/job_weather_back/repository/CustomizationRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.job_weather_back.entity.Customization;
+import com.example.job_weather_back.entity.User;
 
 @Repository
 public interface CustomizationRepository extends JpaRepository<Customization, Integer>{
     // 유저의 맞춤설정 조회
     Optional<Customization> findByUserUserSn(int userSn);
+
+    void deleteByUser(User user);
 }
