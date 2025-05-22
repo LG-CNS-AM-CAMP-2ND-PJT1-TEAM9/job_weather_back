c27f69d39b4429a4d22ce5d706592f88838fd529
Junseo Yoon <58902513+liljuice@users.noreply.github.com>
2025-05-22 17:44:04 +0900

    chore: CORS 설정

--- src/main/java/com/example/job_weather_back/config/WebConfig.java
+++ src/main/java/com/example/job_weather_back/config/WebConfig.java
@@ -11,7 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                    "http://localhost:5173",
+                    "https://good-job-today.netlify.app"
+                    )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); 
